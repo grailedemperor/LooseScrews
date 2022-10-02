ae2d1d625c98a9040c16004de743080387a2d3f6
JC Berry <jcharlesberry@gmail.com>
2022-10-02 15:09:14 -0700

    removed duplicate passport call

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 const methodOverride = require('method-override');
 const ejs = require('ejs');
-const passport = require('passport');
 
 
 const sequelize = require('./config/connection');
